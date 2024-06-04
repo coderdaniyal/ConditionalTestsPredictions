71efbf8ba0ac9a281af40ed7b98201553b8b5214
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-04 22:06:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+var car = "Toyota";
+console.log("Is car == 'toyota'? | predict True");
+console.log(car == "Toyota");
+//Test 2
+var car2 = "Mehran";
+console.log("Is car2 == 'Mehran'? | predict True");
+console.log(car2 == "Mehran");
+//Test 3
+var myName = "Daniyal";
+console.log("Is myName == 'Daniyal'? | predict True");
+console.log(myName == "Daniyal");
+//Test 4
+var myCountry = "Pakistan";
+console.log("Is myCountry == 'Pakistan'? | predict True");
+console.log(myCountry == "Pakistan");
+//Test 5
+var myCity = "Karachi";
+console.log("Is myCity == 'Karachi'? | predict True");
+console.log(myCity == "Karachi");
+//Test 6
+var myAge = 20;
+console.log("Is myAge == 20? | predict True");
+console.log(myAge == 19);
+//Test 7
+var myCountry2 = "Pakistan";
+console.log("Is myCountry2 == Pakistan? | predict True");
+console.log(myCountry2 == "Turkey");
+//Test 8
+var myBike = "cd70";
+console.log("Is myBike == 'cd70'? | predict True");
+console.log(myBike == "125");
+//Test 9
+var myFriend = "umar";
+console.log("Is myFriend == umar? | predict True");
+console.log(myFriend == "ali");
+//Test 10
+var myFriend2 = "rehan";
+console.log("Is myFriend2 == 'rehan'? | predict True");
+console.log(car == "usman");
+//Test 10
+//Test 7
